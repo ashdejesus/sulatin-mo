115b25d6aba82eb82a3e791fad5074594bb2d7b5
NICHOE DE JESUS <dejesusnichoeashley81@gmail.com>
2025-10-02 06:22:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-929985157-ab4ec",
+  "appId": "1:90869043965:web:e3cacc151fcb6be44ee494",
+  "apiKey": "AIzaSyD8LYqZ-ON19Os4HTxO4-dAop7U0GjRFb4",
+  "authDomain": "studio-929985157-ab4ec.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "90869043965"
+};
